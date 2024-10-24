dc09649137fca138bdb7c39c05c01164513bc205
KESHAWN SHARPER <ksharper@studentmba.org>
2024-10-24 15:35:09 -0400

    removed axios

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect,useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./Home.css";
 
 // import { makeStyles } from '@material-ui/core/styles';
